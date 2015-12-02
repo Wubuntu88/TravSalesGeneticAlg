d65139843910c9264691a896de14e95502652a02
William Edward Gillespie <Will@public-68-141.emich.edu>
2015-12-01 19:59:37 -0500

    It now works thanks to Rob's idea of selecting the best from the parent and child groups combined.  It can reach a low number with high mutation rates and many iterations.  In those cases it gets into the low 900s.

--- GeneticAlgCopy.py
+++ GeneticAlgCopy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from City import City
 import random
+import math as math
 import matplotlib.pyplot as plt
 
 __author__ = "will"
@@ -42,28 +43,27 @@ def generate_random_tour(list_of_cities):
     return tour
 
 
-def generate_population(list_of_cities):
+def generate_population(list_of_cities, population_size):
     """
     Generates the initial population of the genetic algorithm
+    :param population_size: (type: Int): the size of the population.
     :param list_of_cities: list of City objects
     :rtype list of a list of City objects.
         a population is a list of tours, which are lists of City objects
     """
-    pop_size = 50
+
     pop = []
-    for i in range(0, pop_size):
+    for i in range(0, population_size):
         random_tour = generate_random_tour(list_of_cities)
         pop.append(random_tour)
     return pop
 
 
-def evolve_population(pop, mutation_rate):
+def classical_selection(pop):
     """
-    :param pop: The population (list of tours, which are lists of City objects).
-    :rtype a population.  Type: list of tours, which are lists of City objects.
+    :param pop:
+    :rtype: object
     """
-    # must create parents
-    # must call and write crossover operator
     population_size = len(pop)
     survivors = []  # the generation undergoes "natural selection"
     tour_costs = []
@@ -77,7 +77,7 @@ def evolve_population(pop, mutation_rate):
         range_tuple = (current_value, current_value + tour_costs[i])
         probability_ranges.append(range_tuple)
         current_value += tour_costs[i]
-
+    # selection part
     # select individuals from population
     assert sum(tour_costs) == current_value
     for i in range(0, population_size):
@@ -87,59 +87,152 @@ def evolve_population(pop, mutation_rate):
             if prob_range[0] <= selection_number <= prob_range[1]:
                 survivors.append(pop[index])
             index += 1
+    assert len(survivors) == population_size
+    # survivors.sort(key=tour_length)
+    return survivors
 
-    assert len(survivors) == len(pop)
-    survivors.sort(key=tour_length)
 
-    next_generation = []
+def cades_selection(pop):
+    survivors = []
+    population_size = len(pop)
+    len_of_tour = len(pop[0])
+    for i in range(0, population_size):
+        selection_range = 10
+        random_index = random.randint(1, population_size - 2 - selection_range)
+        best_tour = pop[random_index]
+        for the_index in range(random_index + 1, random_index + selection_range + 1):
+            if tour_length(pop[the_index]) < tour_length(best_tour):
+                best_tour = pop[the_index]
+        survivors.append(best_tour)
+    return survivors
+
+
+def generate_children(survivors, mutation_rate):
+    population_size = len(survivors)
+    children = []
     for index in range(0, population_size, 2):
         # index is parent1's index, index+1 is parent2's index
         parent1 = survivors[index]
         parent2 = survivors[index+1]
-        # rand = random.random()
-        '''
-        if rand > .5:
-            child1 = cycle_crossover(parent1, parent2)
-            child2 = cycle_crossover(parent2, parent1)
-        else:
-            child1 = crossover(tour1=parent1, tour2=parent2)
-            child2 = crossover(tour1=parent1, tour2=parent2)
-        '''
-        #child1 = cycle_crossover(parent1, parent2)
-        #child2 = cycle_crossover(parent2, parent1)
 
-        child1 = crossover(tour1=parent1, tour2=parent2)
-        child2 = crossover(tour1=parent1, tour2=parent2)
-        # '''
+        child1 = cycle_crossover(parent1, parent2)
+        child2 = cycle_crossover(parent2, parent1)
         if random.random() < mutation_rate:
-                mutate_tour(child1)
+            # mutate_tour(child1)
+            child1 = get_two_opt_swap_mutant(child1)
 
         if random.random() < mutation_rate:
-                mutate_tour(child2)
+            # mutate_tour(child2)
+            child2 = get_two_opt_swap_mutant(child2)
         # '''
-        next_generation.append(child1)
-        next_generation.append(child2)
+        children.append(child1)
+        children.append(child2)
+    return children
+# end of generate_children()
 
-    return next_generation
+
+def evolve_population(pop, mutation_rate):
+    """
+    :param pop: The population (list of tours, which are lists of City objects).
+    :rtype a population.  Type: list of tours, which are lists of City objects.
+    """
+    # must create parents
+    # must call and write crossover operator
+    population_size = len(pop)
+    survivors = classical_selection(pop=pop)
+
+    next_generation = generate_children(survivors, mutation_rate=mutation_rate)
+
+    incestuous_mix = []
+    incestuous_mix.extend(survivors)
+    incestuous_mix.extend(next_generation)
+
+    incestuous_mix.sort(key=tour_length)
+
+    return incestuous_mix[:population_size]
+
+"""
+###--- CROSSOVER OPERATOR METHODS ---###
+"""
+def random_range(lower_limit, upper_limit):
+    start_pos = random.randint(lower_limit, upper_limit)
+    end_pos = random.randint(lower_limit, upper_limit)
+
+    while start_pos == end_pos:
+        end_pos = random.randint(lower_limit, upper_limit)
+    temp = start_pos
+    if start_pos > end_pos:
+        start_pos = end_pos
+        end_pos = temp
+
+    return start_pos, end_pos
+
+
+def index_of_city_in_sub_tour(city, tour, start_index, stop_index):
+    """
+    Finds the index that a city is at in a subtour of an entire tour from
+    start_index to stop_index (exclusive)
+    :param city: city to search for in subtour
+    :param tour: entire tour to search in from start_index to stop_index
+    :param start_index: inclusive start index
+    :param stop_index: exclusive stop index
+    :return: type: Int; index of city in subtour
+    """
+    for index in range(start_index, stop_index):
+        if city is tour[index]:
+            return index
+    return -1
+
+
+def partially_mapped_crossover(parent1, parent2):
+    #the_range = random_range(1, len(parent1) - 2)
+    the_range = 1, 3
+    start_index = the_range[0]
+    end_index = the_range[1]
+
+    # initialization of first child
+    child1 = []
+    child1.extend(parent1[:start_index])
+    child1.extend(parent2[start_index:end_index + 1])
+    child1.extend(parent1[end_index + 1:])
+
+    # initialization of second child
+    child2 = []
+    child2.extend(parent2[:start_index])
+    child2.extend(parent1[start_index:end_index + 1])
+    child2.extend(parent2[end_index + 1:])
+
+    # must make child1 free of duplicates
+    # not done
+
+    return child1, child2
+
+
+def test_partially_mapped_crossover():
+    my_tour1 = [0, 2, 3, 1, 5, 4]
+    my_tour2 = [3, 1, 4, 0, 2, 5]
+    my_child1, my_child2 = partially_mapped_crossover(my_tour1, my_tour2)
+    print("child1: ", my_child1)
+    print("child2: ", my_child2)
 
 
 def cycle_crossover(parent1, parent2):
 
     def index_of(the_city, tour):
         for index in range(1, len(tour) - 1):
-            if tour[index] == the_city:
+            if tour[index] is the_city:
                 return index
         return -1
     len_of_tour = len(parent1)
     child = [None]*len_of_tour
-    random_index = random.randint(1, len_of_tour - 1)
+    random_index = random.randint(1, len_of_tour - 2)
 
     value_at_initial_index_of_parent1 = parent1[random_index]
     value_at_index_of_parent1 = parent1[random_index]
     child[random_index] = value_at_index_of_parent1
     value_at_index_of_parent2 = parent2[random_index]
     set_of_indices = {random_index}
-    while value_at_index_of_parent2 != value_at_initial_index_of_parent1:
+    while value_at_index_of_parent2 is not value_at_initial_index_of_parent1: # I changed from != to is not
         #print("iteration")
         index_in_parent1 = index_of(value_at_index_of_parent2, parent1)
         child[index_in_parent1] = parent1[index_in_parent1]
@@ -157,9 +250,12 @@ def cycle_crossover(parent1, parent2):
 
 def test_cycle_crossover():
     tour1 = [0, 1, 2, 3, 4, 5, 6, 7, 0]
-    tour2 = [0, 5, 4, 6, 7, 2, 3, 1, 0]
+    # tour2 = [0, 5, 4, 6, 7, 2, 3, 1, 0]
+    tour2 = [tour1[0], tour1[5], tour1[4], tour1[6], tour1[7],
+             tour1[2], tour1[3], tour1[1], tour1[-1]]
+
     child = cycle_crossover(tour1, tour2)
-    # child should be [1, 2, 6, 4, 5, 3, 7]
+    # child should be [0, 1, 2, 6, 4, 5, 3, 7, 0] if random index is 1
     print(child)
 
 
@@ -170,8 +266,8 @@ def crossover(tour1, tour2):
     :rtype
     """
     len_of_tours = len(tour1)
-    start_pos = random.randint(1, len_of_tours - 1)
-    end_pos = random.randint(1, len_of_tours - 1)
+    start_pos = random.randint(1, len_of_tours - 2)
+    end_pos = random.randint(1, len_of_tours - 2)
 
     while start_pos == end_pos:
         end_pos = random.randint(1, len_of_tours - 1)
@@ -179,8 +275,7 @@ def crossover(tour1, tour2):
     if start_pos > end_pos:
         start_pos = end_pos
         end_pos = temp
-    # print("start pos: ", start_pos)
-    # print("end pos: ", end_pos)
+
     # now I have gotten the range that will be the crossover section
     # this is the section that will be taken from tour 1 and given to the child
     child_tour = [None] * len_of_tours
@@ -196,7 +291,7 @@ def crossover(tour1, tour2):
         for k in range(1, len_of_tours - 1):
             if child_tour[k] is None:
                 continue
-            if child_tour[k] is tour2[i]:# I CHANGED BECAUSE OF DUPLICATE CITY ERROR; was giving error with tour[i] == tour2[i]
+            if child_tour[k] is tour2[i]:
                 child_contains_city_of_tour2_at_i = True
                 break
         if not child_contains_city_of_tour2_at_i:
@@ -206,7 +301,9 @@ def crossover(tour1, tour2):
                     break
     return child_tour
 
-
+"""
+###--- MUTATE  METHODS ---###
+"""
 def mutate_tour(tour):
     number_of_cities_in_tour = len(tour)
     i = random.randint(1, number_of_cities_in_tour - 2)
@@ -219,32 +316,74 @@ def mutate_tour(tour):
     tour[k] = temp
 
 
-def tour_has_None(tour):
+def two_opt_swap(tour, i, k):
+    """
+    takes the tour input and return a new tour identical to the input tour except the items
+    from i to k are in reverse order
+    :param tour: list of City Objects
+    :param i: first index
+    :param k: second index
+    :rtype a new tour, which is a list of City objects
+    """
+    new_tour = []
+    new_tour.extend(tour[:i])
+    cutlet = tour[i:k]
+    new_tour.extend(cutlet[::-1])
+    new_tour.extend(tour[k:])
+
+    return new_tour
+
+
+def get_two_opt_swap_mutant(tour):
+    number_of_cities_in_a_tour = len(tour)
+    i = random.randint(1, number_of_cities_in_a_tour - 2)
+    k = random.randint(1, number_of_cities_in_a_tour - 1)# was -2
+    if i == number_of_cities_in_a_tour:
+        i -= 1
+
+    if i == k:
+        k += 1
+    elif k < i:
+        temp = i
+        i = k
+        k = temp
+    new_tour = two_opt_swap(tour, i, k)
+    return new_tour
+
+"""Bug detecting method"""
+def tour_has_none(tour):
     for index in range(0, len(tour)):
         if tour[index] is None:
             return True, index
     return False, -1
-#test_cycle_crossover()
 
+"""
+###--- Program Start ---###
+"""
+
+# test_cycle_crossover()
+#test_partially_mapped_crossover()
 
 cities_list = read_in_cities("cities.txt")
-#cities_list = cities_list[:] # breaks
+# testing for smaller sets of cities
+# cities_list = cities_list[:10]
 
-MUTATION_RATE = 0.05
+POPULATION_SIZE = 200
+MUTATION_RATE = 0.01
 
-cost_over_time = []
+cost_over_time = []  # for graphing
 
-population = generate_population(cities_list)
+population = generate_population(cities_list, population_size=POPULATION_SIZE)
 the_best = population[0]
 len_pop = len(population)
 for i in range(1, len_pop - 1):
     if tour_length(population[i]) < tour_length(the_best):
         the_best = population[i]
+
 iterations = 0
-limit = 250
+limit = 1000
 
 while iterations < limit:
-    # print("iteration: ", iterations)
     population = evolve_population(pop=population, mutation_rate=MUTATION_RATE)
 
     if tour_length(population[0]) < tour_length(the_best):
@@ -256,9 +395,16 @@ while iterations < limit:
 for city in the_best:
     print(city)
 print("best tour cost: ", tour_length(the_best))
+print("iterations: ", iterations)
+print("population size", POPULATION_SIZE)
+print("mutation rate: ", MUTATION_RATE)
 
+figure = plt.figure()
 plt.plot(cost_over_time)
-plt.xlabel("Iterations")
-plt.ylabel("Cost")
+figure.suptitle("Genetic Algorithm: Cost of Best Individual at an Iteration", fontsize=20)
+plt.xlabel("Iterations", fontsize=18)
+plt.ylabel("Cost", fontsize=18)
+axes = plt.gca()
+axes.set_xlim([0, len(cost_over_time)])
+axes.set_ylim([0, max(cost_over_time)])
 plt.show()
-
